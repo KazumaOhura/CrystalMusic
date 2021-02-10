a6caa6916e319f3d8793d2cfeabae627d1a4f644
KazumaOhura <k.ohura0303@gmail.com>
2021-02-11 08:34:21 +0900

    [#1] MVVMのテンプレートの追加

---
+++ CrystalMusic/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrystalMusic.ViewModels
+{
+	public class MainViewModel : Helpers.Observable
+	{
+		public Views.MainWindow View { get; private set; } = null;
+
+		public void Initialize(Views.MainWindow mainWindow)
+		{
+			this.View = mainWindow;
+		}
+	}
+}
