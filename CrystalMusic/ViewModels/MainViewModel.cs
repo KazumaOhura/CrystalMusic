f14684c187bc7aaa0e089602f096e7825f50b39d
KazumaOhura <k.ohura0303@gmail.com>
2021-02-11 09:26:01 +0900

    [#4] PlayCommandの追加

--- CrystalMusic/ViewModels/MainViewModel.cs
+++ CrystalMusic/ViewModels/MainViewModel.cs
@@ -4,15 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#if DEBUG
+using Debug;
+#endif
+
 namespace CrystalMusic.ViewModels
 {
 	public class MainViewModel : Helpers.Observable
 	{
 		public Views.MainWindow View { get; private set; } = null;
+		private Models.Player Player { get; set; }
+		private Helpers.RelayCommand playCommand;
+		public Helpers.RelayCommand PlayCommand { get => playCommand = playCommand ?? new Helpers.RelayCommand(OnPlayButtonClicked, Player.CanPlay); }
 
 		public void Initialize(Views.MainWindow mainWindow)
 		{
 			this.View = mainWindow;
+		}
+
+		private void OnPlayButtonClicked()
+		{
+			this.Player.Play();
+			this.View.PlayButton.Visibility = System.Windows.Visibility.Hidden;
+			//this.View.StopButton.Visibility = System.Windows.Visibility.Visible;
+			//this.StopCommand.OnCanExecuteChanged();
+			#if DEBUG
+			DebugConsole.WriteLine("Player.CanPlay():" + this.Player.CanPlay().ToString());
+			//DebugConsole.WriteLine("Player.CanStop():" + this.Player.CanStop().ToString());
+			#endif
 		}
 	}
 }
