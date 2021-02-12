e8a97f19a9fa6ca8a64e68369b9dd095d46e313f
KazumaOhura <k.ohura0303@gmail.com>
2021-02-12 13:24:29 +0900

    [#8] ファイルパスをバインドするプロパティを追加 コマンドの追加。

--- CrystalMusic/ViewModels/MainViewModel.cs
+++ CrystalMusic/ViewModels/MainViewModel.cs
@@ -14,6 +14,7 @@ namespace CrystalMusic.ViewModels
 	{
 		public Views.MainWindow View { get; private set; } = null;
 		private Models.Player Player { get; set; }
+		public string FileName { private get => Player.PlayFileName; set => Player.PlayFileName = value; }
 		private float soundVolume = 50f;
 		public int SoundVolume
 		{
@@ -30,30 +31,47 @@ namespace CrystalMusic.ViewModels
 		public Helpers.RelayCommand StopCommand { get => stopCommand = stopCommand ?? new Helpers.RelayCommand(OnStopButtonClicked, Player.CanStop); }
 		private Helpers.RelayCommand rewindCommand;
 		public Helpers.RelayCommand RewindCommand { get => rewindCommand = rewindCommand ?? new Helpers.RelayCommand(OnRewindButtonClicked); }
+		private Helpers.RelayCommand selectFileCommand;
+		public Helpers.RelayCommand SelectFileCommand { get => selectFileCommand = selectFileCommand ?? new Helpers.RelayCommand(SelectFile); }
 
 		public void Initialize(Views.MainWindow mainWindow)
 		{
 			this.View = mainWindow;
 		}
+		private void SelectFile()
+		{
+			var dialog = new Microsoft.Win32.OpenFileDialog
+			{
+				Filter = "MP3(*.mp3)|*.mp3",
+				Multiselect = true
+			};
+
+			if (dialog.ShowDialog() == true)
+			{
+				this.FileName = dialog.FileName;
+				this.Player.CreateFileReader();
+			}
+			this.PlayCommand.OnCanExecuteChanged();
+		}
 		private void OnPlayButtonClicked()
 		{
 			this.Player.Play();
 			this.View.PlayButton.Visibility = System.Windows.Visibility.Hidden;
-			//this.View.StopButton.Visibility = System.Windows.Visibility.Visible;
-			//this.StopCommand.OnCanExecuteChanged();
+			this.View.StopButton.Visibility = System.Windows.Visibility.Visible;
+			this.StopCommand.OnCanExecuteChanged();
 			#if DEBUG
 			DebugConsole.WriteLine("Player.CanPlay():" + this.Player.CanPlay().ToString());
-			//DebugConsole.WriteLine("Player.CanStop():" + this.Player.CanStop().ToString());
+			DebugConsole.WriteLine("Player.CanStop():" + this.Player.CanStop().ToString());
 			#endif
 		}
 		private void OnStopButtonClicked()
 		{
 			this.Player.Stop();
-			//this.View.PlayButton.Visibility = System.Windows.Visibility.Visible;
+			this.View.PlayButton.Visibility = System.Windows.Visibility.Visible;
 			this.View.StopButton.Visibility = System.Windows.Visibility.Hidden;
-			//this.PlayCommand.OnCanExecuteChanged();
+			this.PlayCommand.OnCanExecuteChanged();
 			#if DEBUG
-			//DebugConsole.WriteLine("Player.CanPlay():" + this.Player.CanPlay().ToString());
+			DebugConsole.WriteLine("Player.CanPlay():" + this.Player.CanPlay().ToString());
 			DebugConsole.WriteLine("Player.CanStop():" + this.Player.CanStop().ToString());
 			#endif
 		}
